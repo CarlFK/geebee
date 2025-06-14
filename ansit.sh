fda2bad9ceafc5a788632ed33eab3ab265dafca3
Carl Karsten <carl@personnelware.com>
2025-06-14 14:33:17 -0500

    Install xfce on _server base.

--- ansit.sh
+++ ansit.sh
@@ -5,10 +5,16 @@ host=${1:-opi}
 
 # export ANSIBLE_STRATEGY=mitogen_linear
 
+# export ANSIBLE_ROLES_PATH=/home/carl/src/dc/debian/ansible/roles/
+# above will override ./ansible.cfg:
+# roles_path=
+
+
 ansible-playbook \
     ansible/site.yml \
     -vvv \
     --inventory-file inventory/hosts \
     --vault-password-file ~/.ansible-vault \
     --user root --limit ${host} \
-    --tags test_one_voc
+
+    # --tags rmdocker
