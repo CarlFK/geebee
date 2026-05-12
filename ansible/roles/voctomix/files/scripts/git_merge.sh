94bcb8500418960d1e82a401761c5aeef2bcc044
Carl Karsten <carl@personnelware.com>
2026-05-12 17:42:03 -0500

    appease git merge Committer identity unknown

--- ansible/roles/voctomix/files/scripts/git_merge.sh
+++ ansible/roles/voctomix/files/scripts/git_merge.sh
@@ -3,9 +3,14 @@
 # merge a bunch of brances into main
 # good for testing pending PRs
 
-git switch main
+date |tee --append git_merge.log
+
+git config --global user.email "you@example.com"|tee --append git_merge.log
+git config --global user.name "Your Name"|tee --append git_merge.log
+
+git switch main |tee --append git_merge.log
 
 while (( "$#" )); do
-    git merge ${1}
+    git merge ${1}|tee --append git_merge.log
     shift
 done
